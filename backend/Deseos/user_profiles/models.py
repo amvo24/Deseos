3bcd2321cc7a20ace8a1b41a14eaebabffeb3a44
Andrew Vo <andrewvo@Andrews-MacBook-Pro-2.local>
2023-12-14 18:33:01 -0600

    added django fields to my user_profiles model and created a virtual env and downloaded django

--- backend/Deseos/user_profiles/models.py
+++ backend/Deseos/user_profiles/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import date
 
 # Create your models here.
+class User_Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    dob = models.DateField(null=True, blank=True, default=date.today)
+    profile_pic = models.ImageField()
+    
