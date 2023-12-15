c2f7f071f47a673772763790558a9c884fbd74c8
Andrew <andrewvo1024@gmail.com>
2023-12-14 19:37:40 -0600

    finished models for user_profiles

--- backend/Deseos/user_profiles/models.py
+++ backend/Deseos/user_profiles/models.py
@@ -6,5 +6,12 @@ from datetime import date
 class User_Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     dob = models.DateField(null=True, blank=True, default=date.today)
-    profile_pic = models.ImageField()
-    
+    profile_pic = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+    street_address = models.CharField(max_length=255, blank=True, null=True)
+    city = models.CharField(max_length=100, blank=True, null=True)
+    state = models.CharField(max_length=50, blank=True, null=True)
+    postal = models.CharField(max_length=20, blank=True, null=True)
+
+
+    def __str__(self):
+        return self.user.username
